5e8e522133c9ee1f2ac7f2993cbcb63a1159711d
pablo.acevedo.m <pablo.acevedo.m@7ae2da1c-ffad-11de-9748-a16e6a44286a>
2010-01-16 10:00:40 +0000

    3 juegos, heuristica implementada en puzzle8 pero aun no funciona, por hacer en las garrafas

---
+++ CasinoIAIC/src/Modelo/Juegos/Garrafas.java
@@ -0,0 +1,204 @@
+package Modelo.Juegos;
+/* FALTA LA HEURISTICA */
+import java.util.Vector;
+
+/**
+ * Juego de las garrafas. Tenemos 2 garrafas, una de 4L y otra de 3L. El juego consiste en obtener 2L en la garrafa grande y 0L en la pequeña.
+ * @author Pablo Acevedo
+ *
+ */
+public class Garrafas extends Juego{
+	/**
+	 * gra guarda el numero de litros que contiene la garrafa grande
+	 */
+	private int gra;
+	/**
+	 * peq guarda el numero de litros que contiene la garrafa pequeña
+	 */
+	private int peq;
+	
+	/**
+	 * Constructora de estado inicial. Pone todo a 0.
+	 */
+	public Garrafas(){
+		gra=0;
+		peq=0;
+		valorHeur=0;
+		coste=0;
+		profundidad=0;
+		camino="";
+	}
+	
+	/**
+	 * Constructora de sucesor. Solo se usa cada vez que se va a crear un sucesor. Obtiene los valores del estado
+	 * padre, y los incrementa de forma adecuada para el árbol de búsqueda.
+	 * 
+	 * @param g estado padre
+	 * @param cos coste acumulado hasta este estado
+	 * @param cam camino para llegar a este estado
+	 */
+	private Garrafas(Garrafas g,int cos,String cam){
+		gra=g.gra;
+		peq=g.peq;
+		coste=g.coste+cos;
+		profundidad=g.profundidad+1;
+		camino=g.camino+cam;
+	}
+
+	
+	/**
+	 * Exploración del estado, según el operador que se quiera aplicar, se devuelve si sería posible o no hacer esa operación.
+	 * 
+	 * @param s operador que se quiere aplicar
+	 * @return true si se puede, false si no
+	 */
+	private boolean puedo(String s){
+		// Llenar grande
+		if (s.equals("LG")){
+			if (gra<4)
+				return true;
+		}
+		// Llenar pequeña
+		if (s.equals("LP")){
+			if (peq<3)
+				return true;
+		}
+		// Vaciar grande
+		if (s.equals("VG")){
+			if (gra>0)
+				return true;
+		}
+		// Vaciar pequeña
+		if (s.equals("VP")){
+			if (peq>0)
+				return true;
+		}
+		// Trasvase de pequeña a grande
+		if (s.equals("PG")){
+			if (peq>0 && gra<4)
+				return true;
+		}
+		// Trasvase de grande a pequeña
+		if (s.equals("GP")){
+			if (gra>0 && peq<3)
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Operador que llena la garrafa grande
+	 */
+	private void llenaG(){
+		gra=4;
+	}
+	
+	/**
+	 * Operador que llena la garrafa pequeña
+	 */
+	private void llenaP(){
+		peq=3;
+	}
+	
+	/**
+	 * Operador que vacía la garrafa grande
+	 */
+	private void vaciaG(){
+		gra=0;
+	}
+	
+	/**
+	 * Operador que vacia la garrafa pequeña
+	 */
+	private void vaciaP(){
+		peq=0;
+	}
+	
+	/**
+	 * Operador que trasvasa la garrafa pequeña a la grande
+	 */
+	private void PtoG(){
+		int cantidad=4-gra;
+		if (peq>=cantidad){
+			gra=4;
+			peq-=cantidad;
+		}
+		else{
+			peq=0;
+			gra+=peq;
+		}
+	}
+	
+	/**
+	 * Operador que trasvasa la garrafa grande a la pequeña
+	 */
+	private void GtoP(){
+		int cantidad=3-peq;
+		if (gra>=cantidad){
+			peq=3;
+			gra-=cantidad;
+		}
+		else{
+			peq+=gra;
+			gra=0;
+		}
+	}
+	
+	public Vector expandir(){
+		Vector<Garrafas> sucesores=new Vector<Garrafas>(0,1);
+		Garrafas estado;
+		if (this.puedo("LG")){
+			estado=new Garrafas(this,1,"Llena grande\n");
+			estado.llenaG();
+			sucesores.add(estado);
+		}
+		if (this.puedo("LP")){
+			estado=new Garrafas(this,1,"Llena pequeña\n");
+			estado.llenaP();
+			sucesores.add(estado);
+		}
+		if (this.puedo("GP")){
+			estado=new Garrafas(this,1,"Grande a pequeña\n");
+			estado.GtoP();
+			sucesores.add(estado);
+		}
+		if (this.puedo("PG")){
+			estado=new Garrafas(this,1,"Pequeña a grande\n");
+			estado.PtoG();
+			sucesores.add(estado);
+		}
+		if (this.puedo("VG")){
+			estado=new Garrafas(this,1,"Vacia grande\n");
+			estado.vaciaG();
+			sucesores.add(estado);
+		}
+		if (this.puedo("VP")){
+			estado=new Garrafas(this,1,"Vacia pequeña\n");
+			estado.vaciaP();
+			sucesores.add(estado);
+		}
+		return sucesores;
+	}
+	
+	public void setGoal(){
+		gra=2;
+		peq=0;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this==o)
+			return true;
+		if ((o==null) || (this.getClass()!=o.getClass()))
+			return false;
+		Garrafas estado=(Garrafas)o;
+		return (estado.gra==gra) && (estado.peq==peq);
+		
+	}
+	
+	@Override
+	public String toString(){
+		return "G="+gra+"L P="+peq+"L\n";
+	}
+	
+}
---
+++ CasinoIAIC/src/Modelo/Juegos/Misioneros.java
@@ -0,0 +1,231 @@
+package Modelo.Juegos;
+
+import java.util.Vector;
+
+/**
+ * Juego de los misioneros y los caníbales. Tenemos 2 orillas, un barco, y un grupo de caníbales y misioneros que quieren cruzar de orilla. En el barco solo
+ * caben 2 personas, y si en una orilla hay menos misioneros que caníbales, estos corren peligro.
+ * @author Pablo Acevedo
+ *
+ */
+
+public class Misioneros extends Juego{
+	/**
+	 * NMI= número de misioneros en la orilla izquierda
+	 */
+	private int NMI;
+	/**
+	 * NCI= número de caníbales en la orilla izquierda
+	 */
+	private int NCI;
+	/**
+	 * BAR= orilla donde se encuentra el barco. 0=derecha, 1=izquierda
+	 */
+	private int BAR;
+	
+	/**
+	 * Constructora del estado inicial, pone todo a 0 y el estado al inicio del juego
+	 */
+	public Misioneros(){
+		NMI=3;
+		NCI=3;
+		BAR=1;
+		valorHeur=-1; // no tiene heuristica
+		coste=0;
+		profundidad=0;
+		camino="";
+	}
+	
+	/**
+	 * Constructora de sucesor. Sólo se usa cuando se va a crear un sucesor, calcula los valores del nuevo estado. El numero de misioneros,
+	 * caníbales y barco se calculan en los operadores.
+	 * @param m estado padre
+	 * @param cos coste acumulado
+	 * @param cam camino acumulado
+	 */
+	private Misioneros(Misioneros m,int cos,String cam){
+		NMI=m.NMI;
+		NCI=m.NCI;
+		BAR=m.BAR;
+		coste=m.coste+cos;
+		profundidad=m.profundidad+1;
+		camino=m.camino+cam;
+	}
+	
+	/**
+	 * Cálculo de los estados de peligro, que se dan si hay menos misioneros que caníbales en alguna orilla
+	 * @param m número de misioneros a la izquierda
+	 * @param c número de caníbales a la izquierda
+	 * @return true si hay peligro, false si no lo hay
+	 */
+	private boolean peligro(int m,int c){
+		return (m<c && m>0)||(m>c && m<3);
+	}
+	
+	/**
+	 * Exploración del estado, según el operador que se quiera aplicar, se devuelve si sería posible o no hacer esa operación.
+	 * 
+	 * @param s operación que se quiere aplicar
+	 * @return true si la operación es posible, false si no lo es
+	 */
+	private boolean puedo(String s){
+		// Mover 2 misioneros
+		if (s.equals("MM")){
+			if (BAR==1)
+				return NMI>1 && !peligro(NMI-2,NCI);
+			else
+				return NMI<2 && !peligro(NMI+2,NCI);
+		}
+		// Mover 2 caníbales
+		if (s.equals("CC")){
+			if (BAR==1)
+				return NCI>1 && !peligro(NMI,NCI-2);
+			else
+				return NCI<2 && !peligro(NMI,NCI+2);
+		}
+		// Mover 1 misionero y 1 caníbal
+		if (s.equals("MC")){
+			if (BAR==1)
+				return NMI>0 && NCI>0 && !peligro(NMI-1,NCI-1);
+			else
+				return NMI<3 && NCI<3 && !peligro(NMI+1,NCI+1);
+		}
+		// Mover 1 caníbal
+		if (s.equals("C")){
+			if (BAR==1)
+				return NCI>0 && !peligro(NMI,NCI-1);
+			else
+				return NCI<3 && !peligro(NMI,NCI+1);
+		}
+		// Mover 1 misionero
+		if (s.equals("M")){
+			if (BAR==1)
+				return NMI>0 && !peligro(NMI-1,NCI);
+			else
+				return NMI<3 && !peligro(NMI+1,NCI);
+		}
+		return false;
+	}
+
+	/**
+	 * Operador que mueve de orilla a 2 misioneros
+	 */
+	private void mueveMM(){
+		if (BAR==1){
+			NMI-=2;
+			BAR=0;
+		}
+		else{
+			NMI+=2;
+			BAR=1;
+		}
+	}
+	
+	/**
+	 * Operador que mueve de orilla a 1 misionero
+	 */
+	private void mueveM(){
+		if (BAR==1){
+			NMI--;
+			BAR=0;
+		}
+		else{
+			NMI++;
+			BAR=1;
+		}
+	}
+	
+	/**
+	 * Operador que mueve de orilla a 1 misionero y 1 caníbal
+	 */
+	private void mueveMC(){
+		if (BAR==1){
+			NMI--;
+			NCI--;
+			BAR=0;
+		}
+		else{
+			NMI++;
+			NCI++;
+			BAR=1;
+		}
+	}
+	
+	/**
+	 * Operador que mueve de orilla a 2 caníbales
+	 */
+	private void mueveCC(){
+		if (BAR==1){
+			NCI-=2;
+			BAR=0;
+		}
+		else{
+			NCI+=2;
+			BAR=1;
+		}
+	}
+	
+	/**
+	 * Operador que mueve de orilla a 1 caníbal
+	 */
+	private void mueveC(){
+		if (BAR==1){
+			NCI--;
+			BAR=0;
+		}
+		else{
+			NCI++;
+			BAR=1;
+		}
+	}
+	
+	public Vector expandir(){
+		Vector<Misioneros> sucesores=new Vector<Misioneros>(0,1);
+		Misioneros estado;
+		if (this.puedo("M")){
+			estado=new Misioneros(this,1,"Mover 1 misionero\n");
+			estado.mueveM();
+			sucesores.add(estado);
+		}
+		if (this.puedo("MM")){
+			estado=new Misioneros(this,1,"Mover 2 misioneros\n");
+			estado.mueveMM();
+			sucesores.add(estado);
+		}
+		if (this.puedo("MC")){
+			estado=new Misioneros(this,1,"Mover 1 misionero y 1 caníbal\n");
+			estado.mueveMC();
+			sucesores.add(estado);
+		}
+		if (this.puedo("CC")){
+			estado=new Misioneros(this,1,"Mover 2 caníbales\n");
+			estado.mueveCC();
+			sucesores.add(estado);
+		}
+		if (this.puedo("C")){
+			estado=new Misioneros(this,1,"Mover 1 caníbal\n");
+			estado.mueveC();
+			sucesores.add(estado);
+		}
+		return sucesores;
+	}
+	
+	public void setGoal(){
+		NMI=0;
+		NCI=0;
+		BAR=0;
+	}
+	
+	public boolean equals(Object o){
+		if (this==o)
+			return true;
+		if ((o==null) || (this.getClass()!=o.getClass()))
+			return false;
+		Misioneros estado=(Misioneros)o;
+		return (NMI==estado.NMI && NCI==estado.NCI && BAR==estado.BAR);
+	}
+	
+	public String toString(){
+		return "Misioneros izquierda: "+NMI+" Canibales izquierda: "+NCI+"\n";
+	}
+}
