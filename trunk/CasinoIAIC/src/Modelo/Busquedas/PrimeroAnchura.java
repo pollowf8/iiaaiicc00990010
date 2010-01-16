26690880fa19f02e46eeb9b980cc1d10e1e3f52d
pablo.acevedo.m <pablo.acevedo.m@7ae2da1c-ffad-11de-9748-a16e6a44286a>
2010-01-16 09:59:38 +0000

    4 busquedas no informadas

---
+++ trunk/CasinoIAIC/src/Modelo/Busquedas/PrimeroAnchura.java
@@ -0,0 +1,70 @@
+package Modelo.Busquedas;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Vector;
+import Modelo.Juegos.Juego;
+import Modelo.Juegos.Misioneros;
+
+/**
+ * Búsqueda primero en anchura. Desde un estado inicial va expandiendo nodos y recorriendolos con una cola, de forma
+ * que se busca la solucion por niveles. Incluye control de ciclos.
+ * @author Pablo Acevedo
+ * 
+ */
+public class PrimeroAnchura implements Busqueda{
+
+	@Override
+	public Juego resuelve(Juego inicial,Juego goal){
+		boolean fin=false;
+		int generados=1;
+		int expandidos=0;
+		List<Juego> listaCerrados=new Vector<Juego>(0,1);
+		Queue<Juego> colaAbiertos=new LinkedList<Juego>();
+		colaAbiertos.offer(inicial);
+		Juego juego=null;
+		while (!fin){
+			juego=colaAbiertos.poll();
+			if (juego.equals(goal)){
+				// Si el juego es igual que el estado final, se guarda en su camino toda la información de la búsqueda para mostrarla después
+				fin=true;
+				String camino=juego.getCamino()+"Nodos generados:"+generados+" ; Nodos expandidos:"+expandidos+" ; Coste: "+juego.getCoste()+
+								" ; Profundidad:"+juego.getProfundidad();
+				juego.setCamino(camino);
+			}
+			else{
+				// como no es estado final, se cierra, ya no se va a mirar más
+				listaCerrados.add(juego);
+				// expandir un nuevo nodo
+				expandidos++;
+				// sucesores del nodo que se acaba de cerrar
+				Vector<Juego> sucesores=juego.expandir();
+				// si uno de los sucesores ya aparece en la lista de nodos cerrados, no se añade a la cola de abiertos, así se evitan ciclos
+				for (int i=0;i<sucesores.size();i++){
+					if (!listaCerrados.contains(sucesores.elementAt(i))){
+						colaAbiertos.offer(sucesores.elementAt(i));
+						generados++;
+					}
+				}
+			}
+		}
+		return juego;
+	}
+	
+	/**
+	 * Main para probar la búsqueda
+	 * @param args, no se usa
+	 */
+	public static void main(String[] args){
+		Misioneros inicial=new Misioneros();
+		Misioneros goal=new Misioneros();
+		goal.setGoal();
+		PrimeroAnchura busqueda=new PrimeroAnchura();
+		Misioneros solucion=(Misioneros)busqueda.resuelve(inicial,goal);
+		System.out.print("Búsqueda en anchura Misioneros:\nEstado inicial: "+inicial.toString());
+		System.out.print(solucion.getCamino());
+		System.out.print("\nSolución: "+solucion.toString());
+	}
+	
+}
---
+++ trunk/CasinoIAIC/src/Modelo/Busquedas/ProfundidadIterativa.java
@@ -0,0 +1,89 @@
+package Modelo.Busquedas;
+
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+import Modelo.Juegos.Juego;
+import Modelo.Juegos.Puzzle8;
+
+/**
+ * Búsqueda en profundidad iterativa. Sigue el mismo principio que la búsqueda en profundidad limitada, empieza con una profundidad 0, y va incrementandola
+ * haciendo tantas iteraciones como necesite para encontrar la solución. Incluye control de ciclos, errores y excepciones.
+ * @author Pablo Acevedo
+ *
+ */
+public class ProfundidadIterativa implements Busqueda{
+
+	@Override
+	public Juego resuelve(Juego inicial,Juego goal){
+		boolean fin=false;
+		int generados=1;
+		int expandidos=0;
+		int profundidad=0;
+		List<Juego> listaCerrados=new Vector<Juego>(0,1);
+		Stack<Juego> pilaAbiertos=new Stack<Juego>();
+		pilaAbiertos.push(inicial);
+		Juego juego=null;
+		// se hace try&catch porque ésta búsqueda puede no acabar por falta de memoria. imposible por los juegos que se van a usar? A REVISAR
+		try{
+			while (!fin){
+				// si no hay elementos en la pila quiere decir que se ha recorrido el árbol entero sin encontrar la solución. Se incrementa en 1 la profundidad,
+				// se borra la lista de nodos cerrados y se vuelve a empezar desde el nodo inicial.
+				if (pilaAbiertos.size()==0){
+					listaCerrados.clear();
+					pilaAbiertos.push(inicial);
+					profundidad++;
+				}
+				juego=pilaAbiertos.pop();
+				if (juego.equals(goal)){
+					// Si el juego es igual que el estado final, se guarda en su camino toda la información de la búsqueda para mostrarla después
+					fin=true;
+					String camino=juego.getCamino()+"Nodos generados:"+generados+" ; Nodos expandidos:"+expandidos+" ; Coste: "+juego.getCoste()+
+								" ; Profundidad:"+juego.getProfundidad();
+					juego.setCamino(camino);
+				}
+				else{
+					// como no es estado final, se cierra, ya no se va a mirar más
+					listaCerrados.add(juego);
+					// expandir un nuevo nodo
+					expandidos++;
+					// sucesores del nodo que se acaba de cerrar
+					Vector<Juego> sucesores=juego.expandir();
+					// por ser una pila se meten al revés, para recorrerlos en el orden en que se aplican los operadores. Si un sucesor ya estaba en la
+					// lista de cerrados, no se introduce en la pila, para evitar ciclos/ramas infinitas. Además, si la profundidad del nodo que se acaba de
+					// cerrar es mayor que la profundidad límite, tampoco se introducen sus sucesores en la pila.
+					for (int i=sucesores.size();i>0;i--){
+						if (!listaCerrados.contains(sucesores.elementAt(i-1))&&juego.getProfundidad()<profundidad){
+							pilaAbiertos.push(sucesores.elementAt(i-1));
+							generados++;
+						}
+					}
+				}
+			}
+			return juego;
+		}
+		catch (Error e){
+			return null;
+		}
+	}
+	
+	/**
+	 * Main para probar la búsqueda
+	 * @param args, no se usa
+	 */
+	public static void main(String[] args){
+		Puzzle8 inicial=new Puzzle8();
+		Puzzle8 goal=new Puzzle8();
+		goal.setGoal();
+		ProfundidadIterativa busqueda=new ProfundidadIterativa();
+		Puzzle8 solucion=(Puzzle8)busqueda.resuelve(inicial,goal);
+		System.out.print("Búsqueda en profundidad iterativa Puzzle8:\nEstado inicial: "+inicial.toString());
+		if (solucion!=null){
+			System.out.print(solucion.getCamino());
+			System.out.print("\nSolución: "+solucion.toString());
+		}
+		else
+			System.out.print("No se encontró ninguna solución");
+	}
+}
