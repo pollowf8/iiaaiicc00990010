c9c141640f0e5b89fd45e24f96a1c12f268540c7
pablo.acevedo.m <pablo.acevedo.m@7ae2da1c-ffad-11de-9748-a16e6a44286a>
2010-01-18 21:48:05 +0000

    Enfriamiento simulado y escalada simple añadidos

---
+++ trunk/CasinoIAIC/src/Modelo/Busquedas/EnfriamientoSimulado.java
@@ -0,0 +1,76 @@
+package Modelo.Busquedas;
+
+import java.util.Random;
+import java.util.Vector;
+import Modelo.Juegos.*;
+
+public class EnfriamientoSimulado implements Busqueda{
+
+	@Override
+	public Juego resuelve(Juego inicial) {
+		if (inicial.getValorHeur()==-1 || inicial.isGoal())
+			return inicial;
+		Random rand=new Random();
+		int r;
+		int generados=1;
+		int expandidos=1;
+		Juego actual=inicial;
+		Juego mejorHastaAhora=actual;
+		double T=1;
+		Vector<Juego> sucesores=new Vector<Juego>(0,1);
+		sucesores=inicial.expandir();
+		generados+=sucesores.size();
+		while (!sucesores.isEmpty() && T>0){
+			r=rand.nextInt(sucesores.size());
+			Juego nuevoEstado=sucesores.elementAt(r);
+			double e=nuevoEstado.getValorHeur()-actual.getValorHeur();
+			if (nuevoEstado.isGoal()){
+				String camino=nuevoEstado.getCamino()+"Nodos generados:"+generados+" ; Nodos expandidos:"+expandidos+" ; Coste: "+nuevoEstado.getCoste()+
+				" ; Profundidad:"+nuevoEstado.getProfundidad();
+				nuevoEstado.setCamino(camino);
+				return nuevoEstado;
+			}
+			else{
+				if (nuevoEstado.getValorHeur()<actual.getValorHeur()){
+					actual=nuevoEstado;
+					if (nuevoEstado.getValorHeur()<mejorHastaAhora.getValorHeur())
+						mejorHastaAhora=nuevoEstado;
+					sucesores.clear();
+					sucesores=nuevoEstado.expandir();
+					expandidos++;
+					generados+=sucesores.size();
+				}
+				else{
+					double P=Math.pow((double)Math.E,(double)-e/T);
+					double N=rand.nextDouble();
+					if (N<P){
+						actual=nuevoEstado;
+						sucesores.clear();
+						sucesores=nuevoEstado.expandir();
+						expandidos++;
+						generados+=sucesores.size();
+					}
+					else
+						sucesores.remove(r);
+				}
+			}
+			T-=0.001;
+		}
+		return mejorHastaAhora;
+	}
+	
+	public static void main(String[] args){
+		Juego inicial=new Puzzle8();
+		EnfriamientoSimulado busqueda=new EnfriamientoSimulado();
+		System.out.println("Enfriamiento simulado:\n");
+		System.out.println("Inicial: "+inicial.toString());
+		Juego solucion=(Puzzle8)busqueda.resuelve(inicial);
+		if (solucion.isGoal()){
+			System.out.println(solucion.getCamino());
+			System.out.println("Solución: "+solucion.toString());
+		}
+		else
+			System.out.println("No se encontró solución");
+	}
+	
+}
